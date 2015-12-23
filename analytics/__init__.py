5766fe1c090b53b07700824b1c55a954db16c6e8
Buck Baskin <mobile.wbaskin@gmail.com>
2015-12-22 23:13:12 -0500

    Adding analytics/tracking extensions

---
+++ analytics/__init__.py
@@ -0,0 +1,1 @@
+from engagement import trace as trace
---
+++ analytics/engagement.py
@@ -0,0 +1,55 @@
+import datetime
+
+def save_call(fname, params=(), args=(), vargs={}, ret_val=None):
+    arg_queue = list(args)
+    varg_queue = dict(vargs)
+    out_queue = []
+
+    print 'lp: '+str(len(params))
+
+    for i in range(0,len(params)):
+        print 'i: '+str(i)
+        param = params[i]
+        if len(arg_queue) > 0:
+            if i == (len(params)-1):
+                print 'aq: '+str(arg_queue)
+                print 'vq: '+str(varg_queue)
+                arg = arg_queue
+                # print 'pair: '+str((param, arg,))
+                out_queue.append((param, arg,))
+                # varg = varg_queue
+                # out_queue.append((param, varg,))
+            else:
+                arg = arg_queue.pop(0)
+                out_queue.append((param, arg,))
+        elif param in varg_queue:
+            arg = varg_queue.pop(param)
+            out_queue.append((param, arg,))
+        else:
+            # if i == (len(params)-1):
+            #     print 'vq: '+str(varg_queue)
+            #     varg = varg_queue
+            #     out_queue.append((param, varg,))
+            # elif len(arg_queue) == 0:
+            #     arg = arg_queue
+            #     # print 'pair: '+str((param, arg,))
+            #     out_queue.append((param, arg,))
+            # else:
+                print 'param not matched: '+str(param)
+
+    print str(datetime.datetime.now())
+    print str(fname)+'('+str(out_queue)+')'+' => '+str(ret_val)
+
+
+def trace(function):
+    def wrapped_function(*args, **vargs):
+        call_name = function.func_name
+        call_params = function.__code__.co_varnames
+        call_args = args
+        call_vargs = vargs
+
+        val = function(*args, **vargs)
+        
+        save_call(fname=call_name, params=call_params, args=call_args, vargs=call_vargs, ret_val=val)
+        return val
+    return wrapped_function
