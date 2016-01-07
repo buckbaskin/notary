abd16df3bf5b0840d766abe77792c99cd2ca0447
Buck Baskin <mobile.wbaskin@gmail.com>
2016-01-07 13:38:53 -0500

    Main.js, login.js converted to  based operation. See #26

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,88 +1,17 @@
-var authToken = "";
-var hideConfirmBool = true;
-const hideConfirmText = "<input type=\"password\" class=\"form-control\" id=\"confirm-password\" placeholder=\"confirm password\" onblur=\"checkConfirmMatch();\">";
+function login(old_scope) {
+  $cope = clone(old_scope)
+  $cope.authToken = "";
+  $cope.hideConfirmBool = true;
+  $cope.hideConfirmText = "<input type=\"password\" class=\"form-control\" id=\"confirm-password\" placeholder=\"confirm password\" onblur=\"$cope.checkConfirmMatch();\">";
 
-function sendLogin() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+  $cope.sendLogin = function() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-  if (username.length > 0 && password.length) {
-    var myRequest = {
-      'atoken': [username,''],
-      'action': 'login',
-      'username': username,
-      'password': password
-    }
-
-    var myJson = JSON.stringify(myRequest);
-
-    request("POST", "json", "/u.json", function(res) {
-      console.log("response");
-      console.log(response);
-      if (res.readyState === 4 && res.status === 200) {
-          var response = JSON.parse( res.responseText );
-          if (response !== "") {
-            console.log("response");
-            console.log(response);
-            authToken = response;
-          } else {
-            document.getElementById("password").value = "";
-            document.getElementById("message").innerHTML = "Invalid username or password";
-          }
-        }
-    }, myJson);
-  } else {
-    document.getElementById("message").innerHTML = "Please enter a username and password";
-  }
-}
-
-function hideConfirm() {
-  console.log("hideConfirm");
-  hideConfirmBool = true;
-  var oldtext = document.getElementById("confirm-holder").innerHTML;
-  document.getElementById("confirm-holder").innerHTML = "";
-}
-
-function showConfirm() {
-  console.log("showConfirm");
-  hideConfirmBool = false;
-  document.getElementById("confirm-holder").innerHTML = hideConfirmText;
-
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  if (username.length > 0 && password.length > 0) {
-    document.getElementById("message").innerHTML = "Confirm your password to create a new account";
-  }
-}
-
-function checkConfirmMatch() {
-  // TODO(buckbaskin)
-  console.log("checkConfirmMatch");
-  var password = document.getElementById("password").value;
-  var confirmPassword = document.getElementById("confirm-password").value;
-  if (password === confirmPassword) {
-    document.getElementById("confirm-password").style['background-color'] = "green";
-    return true;
-  } else {
-    document.getElementById("confirm-password").style['background-color'] = "red";
-    return false;
-  }
-}
-
-function createUser() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-
-  if (hideConfirmBool) {
-    showConfirm();
-  } else {
     if (username.length > 0 && password.length) {
       var myRequest = {
-        'atoken': [
-          username,
-          '',
-        ],
-        'action': 'create',
+        'atoken': [username,''],
+        'action': 'login',
         'username': username,
         'password': password
       }
@@ -90,12 +19,14 @@ function createUser() {
       var myJson = JSON.stringify(myRequest);
 
       request("POST", "json", "/u.json", function(res) {
+        console.log("response");
+        console.log(response);
         if (res.readyState === 4 && res.status === 200) {
             var response = JSON.parse( res.responseText );
             if (response !== "") {
               console.log("response");
               console.log(response);
-              var new_user_id = response;
+              authToken = response;
             } else {
               document.getElementById("password").value = "";
               document.getElementById("message").innerHTML = "Invalid username or password";
@@ -106,4 +37,80 @@ function createUser() {
       document.getElementById("message").innerHTML = "Please enter a username and password";
     }
   }
+
+  $cope.hideConfirm = function() {
+    console.log("hideConfirm");
+    $cope.hideConfirmBool = true;
+    var oldtext = document.getElementById("confirm-holder").innerHTML;
+    document.getElementById("confirm-holder").innerHTML = "";
+  }
+
+  console.log($cope.hideConfirm);
+
+  $cope.showConfirm = function() {
+    console.log("showConfirm");
+    $cope.hideConfirmBool = false;
+    document.getElementById("confirm-holder").innerHTML = $cope.hideConfirmText;
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username.length > 0 && password.length > 0) {
+      document.getElementById("message").innerHTML = "Confirm your password to create a new account";
+    }
+  }
+
+  $cope.checkConfirmMatch = function() {
+    // TODO(buckbaskin)
+    console.log("checkConfirmMatch");
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm-password").value;
+    if (password === confirmPassword) {
+      document.getElementById("confirm-password").style['background-color'] = "green";
+      return true;
+    } else {
+      document.getElementById("confirm-password").style['background-color'] = "red";
+      return false;
+    }
+  }
+
+  $cope.createUser = function() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if ($cope.hideConfirmBool) {
+      $cope.showConfirm();
+    } else {
+      if (username.length > 0 && password.length) {
+        var myRequest = {
+          'atoken': [
+            username,
+            '',
+          ],
+          'action': 'create',
+          'username': username,
+          'password': password
+        }
+
+        var myJson = JSON.stringify(myRequest);
+
+        request("POST", "json", "/u.json", function(res) {
+          if (res.readyState === 4 && res.status === 200) {
+              var response = JSON.parse( res.responseText );
+              if (response !== "") {
+                console.log("response");
+                console.log(response);
+                var new_user_id = response;
+              } else {
+                document.getElementById("password").value = "";
+                document.getElementById("message").innerHTML = "Invalid username or password";
+              }
+            }
+        }, myJson);
+      } else {
+        document.getElementById("message").innerHTML = "Please enter a username and password";
+      }
+    }
+  }
 }
+
+login($cope);
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -14,3 +14,30 @@ function request(request, type, url, action, send) {
 
   xmlhttp.send(send);
 }
+
+function clone(obj) {
+	var copy;
+	if (null == obj || "object" != typeof obj) {
+		return obj;
+	}
+	if (obj instanceof Array) {
+		copy = [];
+		var len = obj.length;
+		for (var i = 0; i < len; i++) {
+			copy[i] = clone(obj[i]);
+		}
+		return copy;
+	}
+	if (obj instanceof Object) {
+		copy = {};
+		for (var attr in obj) {
+			if (obj.hasOwnProperty(attr)) {
+				copy[attr] = clone(obj[attr]);
+			}
+		}
+		return copy;
+	}
+	throw new Error("Unable to copy obj! Its type isn't supported");
+}
+
+var $cope = {};
